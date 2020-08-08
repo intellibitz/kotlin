8c942f64e0548e8d6532ca3ead8f3d441f0ecc01
Muthuselvam Paravai KPR <muthu.ramadoss@gmail.com>
2020-08-08 15:47:42 +0530

    commit KotlinLearn.

--- KotlinLearn/src/main/intellibitz/genric/Genric.kt
+++ KotlinLearn/src/main/intellibitz/genric/Genric.kt
@@ -6,6 +6,20 @@ fun main() {
 //    println(oneHalf("12")
     println(ensureTrailingPeriod(hello))
     println(hello)
+    println(ip.process(2))
+    println(sp.process(null))
+    println(isA<Int>(2))
+}
+
+inline fun <reified T> isA(value: Any) = value is T
+
+val ip: Processor<Int> = Processor()
+val sp = Processor<String?>()
+
+class Processor<T> {
+    fun process(value: T) {
+        value?.hashCode()
+    }
 }
 
 //generic extension property
@@ -20,6 +34,7 @@ fun <T : Number> oneHalf(value: T): Double {
 }
 
 val hello = StringBuilder("Hello World")
+
 // specifies multiple constraints on type parameter (Note: CharSequence, Appendable)
 fun <T> ensureTrailingPeriod(seq: T) where T : CharSequence, T : Appendable {
     if (!seq.endsWith(".")) seq.append(".")
