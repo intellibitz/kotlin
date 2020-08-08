0bc7a2a9301283b9e91ed7ca2c748ab9c89be4ce
Muthuselvam Paravai KPR <muthu.ramadoss@gmail.com>
2020-08-08 15:06:22 +0530

    commit KotlinLearn.

--- KotlinLearn/src/main/intellibitz/genric/Genric.kt
+++ KotlinLearn/src/main/intellibitz/genric/Genric.kt
@@ -2,7 +2,25 @@ package genric
 
 fun main() {
     println(listOf(1, 2, 3).penultimate)
+    println(oneHalf(33))
+//    println(oneHalf("12")
+    println(ensureTrailingPeriod(hello))
+    println(hello)
 }
 
+//generic extension property
 val <T> List<T>.penultimate: T
     get() = this[size - 2]
+//NOTE: generic non extension property, generic regular properties are not supported
+//val <T> x: T = TODO()
+
+fun <T : Number> oneHalf(value: T): Double {
+//    invokes methods defined in upper bound type (Number in this case)
+    return value.toDouble() / 2
+}
+
+val hello = StringBuilder("Hello World")
+// specifies multiple constraints on type parameter (Note: CharSequence, Appendable)
+fun <T> ensureTrailingPeriod(seq: T) where T : CharSequence, T : Appendable {
+    if (!seq.endsWith(".")) seq.append(".")
+}
