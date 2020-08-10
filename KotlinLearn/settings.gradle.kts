6fb9bd1a20ce3463deae465f1a8926d3cc152be5
Muthuselvam Paravai KPR <muthu.ramadoss@gmail.com>
2020-08-10 19:29:12 +0530

    commit KotlinLearn.

--- KotlinLearn/settings.gradle.kts
+++ KotlinLearn/settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         jcenter()
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
         maven("https://plugins.gradle.org/m2/")
+        maven("https://dl.bintray.com/kotlin/dokka")
     }
 }
 
